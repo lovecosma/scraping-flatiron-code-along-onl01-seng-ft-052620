131dfafee8d721fd7543796d14dfad8c8dfdef41
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-16 04:25:27 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
+
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -15,10 +15,9 @@ class Scraper
       end
     end
   end
-  
+
 end
 
 
 def get_page
 end 
-
