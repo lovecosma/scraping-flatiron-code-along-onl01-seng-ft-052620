0f5c9a53b0bed1567c1d66de1c3e045dc49cb81f
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-16 04:41:56 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,40 @@
+class Course
+attr_accessor :title, :schedule, :description
+@@all = []
+def initialize
+  @@all << self
+end
+def title=(title)
+@title = title
+end
+
+def title
+  @title
+end
+
+def schedule=(schedule)
+@schedule = schedule
+end
+
+def schedule
+  @schedule
+end
+
+def description=(description)
+@description = description
+end
+
+def description
+  @description
+end
+
+def self.all
+@@all
+end
+
+def self.reset_all
+@@all.clear
+end
 
 
+end
